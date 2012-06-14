53bbeec898dac006469a20f2d3b9bdc297eb4127
Jell <jean-louis@jawaninja.com>
2012-06-14 18:37:30 +0200

    First commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject junebot "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [aleph "0.3.0-alpha2"]])
---
+++ test/junebot/test/core.clj
@@ -0,0 +1,6 @@
+(ns junebot.test.core
+  (:use [junebot.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
