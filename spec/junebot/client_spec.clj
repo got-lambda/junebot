bc9f1774349c1ba1223bd1bee1f413b22396050f
Jell <jean-louis@jawaninja.com>
2012-11-15 19:49:27 +0100

    Added some tests.

--- spec/junebot/client_spec.clj
+++ spec/junebot/client_spec.clj
@@ -3,12 +3,21 @@
     [speclj.core]
     [junebot.client]))
 
-(describe "Truth"
+(describe "update-world"
 
-  (it "is true"
-    (should true))
+  (with world-state {:walls []  :players []  :shots []})
 
-  (it "is not false"
-    (should-not false)))
+  (it "updates walls on :new-world"
+    (should= {:walls [1 2 3], :players [], :shots []}
+             (update-world @world-state [:new-world [1 2 3]])))
+
+  (it "updates players on :update-players"
+    (should= {:walls [], :players [1 2], :shots []}
+             (update-world @world-state [:update-players [1 2]])))
+
+  (it "updates players on :update-shots"
+    (should= {:walls [], :players [], :shots [1 2]}
+             (update-world @world-state [:update-shots [1 2]])))
+  )
 
 (run-specs)
